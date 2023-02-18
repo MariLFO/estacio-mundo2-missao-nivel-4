f929e503860b7f6b638318e27540df169f412daa
MariLFO <mari.fer.onorio@gmail.com>
2023-02-18 15:41:56 -0800

    Atributo editoras, como Array<Editora>

--- livros-angular/src/app/controle-editora.service.ts
+++ livros-angular/src/app/controle-editora.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Editora } from './editora';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ControleEditoraService {
-
+  editoras: Array<Editora> = [];
   constructor() { }
 }
