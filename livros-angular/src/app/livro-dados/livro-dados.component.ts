a046ff6c89f716c88edcd5f2f3ea1af3e7891dc6
MariLFO <mari.fer.onorio@gmail.com>
2023-02-18 17:07:28 -0800

    Injetando atributos no construtor

--- livros-angular/src/app/livro-dados/livro-dados.component.ts
+++ livros-angular/src/app/livro-dados/livro-dados.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import {Livro} from "../livro";
-import {Editora} from "../editora";
+import { Livro } from "../livro";
+import { Editora } from "../editora";
+import { ControleEditoraService } from "../controle-editora.service";
+import { ControleLivrosService } from "../controle-livros.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-livro-dados',
@@ -12,7 +15,10 @@ export class LivroDadosComponent {
   public autoresForm: string = '';
   public editoras: Array<Editora> = [];
 
-  constructor() {
+  constructor(
+    private servEditora: ControleEditoraService,
+    private servLivros: ControleLivrosService,
+    private router: Router) {
     this.livro = new Livro();
   }
 }
